cfc29de8fced521efe4e1a0098cd4cc067986cf4
madsenmat <madsenmat@gmail.com>
2016-11-26 14:50:55 -0800

    Documented and addded

--- R/mean_dif.R
+++ R/mean_dif.R
@@ -1,3 +1,14 @@
+#' Difference of means of two numeric variables
+#'
+#'Takes the mean of two numeric variables (x and y) then takes the difference between them (x-y).
+#' @param x numeric, of any length
+#' @param y numeric, of any length
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#'#' mean_dif(mtcars$cyl,mtcars$gear)
 mean_dif <- function(x,y) {
   mean1 <- base::mean(x)
   mean2 <- base::mean(y)
