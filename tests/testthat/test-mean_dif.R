86f439552168e771806cd20aa10ba9e356f47967
madsenmat <madsenmat@gmail.com>
2016-12-03 13:30:21 -0800

    Added 3 Tests to Mean_Dif

--- tests/testthat/test-mean_dif.R
+++ tests/testthat/test-mean_dif.R
@@ -8,3 +8,24 @@ test_that("mean_dif takes mean right and substracts", {
 
   expect_identical(mean_dif(x, y), z)
 })
+
+test_that("mean_dif is will work with only Zeros", {
+
+  x <- c(0,0,0)
+  y <- c(0,0,0)
+
+  expect_identical(mean_dif(x,y), 0)
+})
+
+test_that("mean_dif does not compute with NaN", {
+
+  w <- c("Rick", "Bill")
+  z <- c(1,1,1,1,1,1,1,1)
+
+  expect_warning(mean_dif(w,z), "argument is not numeric or logical: returning NA")
+
+  })
+
+
+
+
